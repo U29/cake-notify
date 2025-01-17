f44fc056683c3c2a7fcf233d7de458642761d3f4
U29 <yuluthi@gmail.com>
2025-01-17 23:15:08 +0900

    fix: terraformを修正

---
+++ terraform/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+  profile = "default"
+}
+
+resource "aws_dynamodb_table" "cake_notify" {
+  name           = "cake-notify"
+  billing_mode   = "PAY_PER_REQUEST"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "user_id"
+  range_key      = "birthday"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "birthday"
+    type = "S"
+  }
+}
+
+resource "aws_lambda_function" "cake_notifier" {
+  function_name = "cake-notifier"
+  role          = var.lambda_role_arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.12"
+
+  environment {
+    variables = {
+      DYNAMODB_TABLE = aws_dynamodb_table.cake_notify.name
+    }
+  }
+}
+
+resource "aws_scheduler_schedule" "cake_notify_daily" {
+  name                = "cake-notify-daily"
+  group_name          = "cake-notify"
+  schedule_expression = "cron(0 0 * * ? *)"
+  schedule_expression_timezone = "Asia/Tokyo"
+  state               = "DISABLED"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  target {
+    arn      = aws_lambda_function.cake_notifier.arn
+    role_arn = var.scheduler_role_arn
+
+    retry_policy {
+      maximum_event_age_in_seconds = 86400
+      maximum_retry_attempts       = 185
+    }
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "lambda_role_arn" {
+  type = string
+}
+
+variable "scheduler_role_arn" {
+  type = string
+}
