f63c05b6d6edf95023ffcd6b8d23229c31dd35e2
U29 <yuluthi@gmail.com>
2025-01-18 15:39:20 +0900

    fix: var

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
 resource "aws_dynamodb_table" "cake_notify" {
