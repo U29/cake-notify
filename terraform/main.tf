7fa2afcd3bf271ac62832407373763ada4445b6c
U29 <yuluthi@gmail.com>
2025-01-19 10:11:31 +0900

    feat: terraformのbackendをs3に変更

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "cake-notify-infra"
+    key    = "terraform/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
 }
 
 provider "aws" {
@@ -14,10 +20,10 @@ provider "aws" {
 }
 
 resource "aws_dynamodb_table" "cake_notify" {
-  name           = "cake-notify"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "user_id"
-  range_key      = "birthday"
+  name         = "cake-notify"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "user_id"
+  range_key    = "birthday"
 
   attribute {
     name = "user_id"
@@ -31,27 +37,27 @@ resource "aws_dynamodb_table" "cake_notify" {
 }
 
 resource "aws_lambda_function" "cake_notifier" {
-  function_name = "cake-notifier"
-  role          = var.lambda_role_arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.12"
-  filename      = "${path.module}/../lambda/lambda_function.zip"
+  function_name    = "cake-notifier"
+  role             = var.lambda_role_arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.12"
+  filename         = "${path.module}/../lambda/lambda_function.zip"
   source_code_hash = filebase64sha256("${path.module}/../lambda/lambda_function.zip")
 
   environment {
     variables = {
-      DYNAMODB_TABLE = aws_dynamodb_table.cake_notify.name
+      DYNAMODB_TABLE      = aws_dynamodb_table.cake_notify.name
       DISCORD_WEBHOOK_URL = var.discord_webhook_url
     }
   }
 }
 
 resource "aws_scheduler_schedule" "cake_notify_daily" {
-  name                = "cake-notify-daily"
-  group_name          = "cake-notify"
-  schedule_expression = "cron(0 0 * * ? *)"
+  name                         = "cake-notify-daily"
+  group_name                   = "cake-notify"
+  schedule_expression          = "cron(0 0 * * ? *)"
   schedule_expression_timezone = "Asia/Tokyo"
-  state               = "ENABLED"
+  state                        = "ENABLED"
 
   flexible_time_window {
     mode = "OFF"
